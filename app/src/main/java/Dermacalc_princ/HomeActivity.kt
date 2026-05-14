7c7a780ae3146b22bb9aa98d8dfd1d4768abcd47
Gaia <gpellegrini3@studenti.uninsubria.it>
2026-05-14 14:38:13 +0200

    login e log out funzionante almeno a livello grafico

--- app/src/main/java/Dermacalc_princ/HomeActivity.kt
+++ app/src/main/java/Dermacalc_princ/HomeActivity.kt
@@ -1,6 +1,8 @@
 package Dermacalc_princ
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.Dermcalc_princ.R
 
@@ -8,5 +10,16 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        btnLogout.setOnClickListener {
+            // Torna alla MainActivity (Login)
+            val intent = Intent(this, MainActivity::class.java)
+            // Pulisce lo stack delle activity per evitare di tornare indietro
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
